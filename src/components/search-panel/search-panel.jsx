54292991e8d1575bbfcde0b1efcd0eb02f3bb2f9
Serhii <idaveee@gmail.com>
2021-03-27 21:21:28 +0300

    starting here. with some POST-APP

---
+++ src/components/search-panel/search-panel.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SearchPanel = () => {
+	return (
+		<input 
+			type="text"
+			className="form-control search-input"
+			placeholder="Поиск по записям"
+		/>
+	);
+};
+
+export default SearchPanel;
